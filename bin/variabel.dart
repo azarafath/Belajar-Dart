10d15665bb9d3739648303d7e08cd1099fc85936
azarafath <ahmadzakariafathoni@gmail.com>
2022-07-07 21:25:23 +0700

    dart part 1

---
+++ bin/variabel.dart
@@ -0,0 +1,12 @@
+void main() {
+  String name = 'Ahmad Zakaria Fathoni';
+  print(name);
+  print(name);
+  print(name);
+  const nama = 'Ahmad Zakaria Fathoni';
+  print(nama);
+  print(nama);
+  final namaa = 'Ahmad Zakaria Fathoni';
+  print(namaa);
+  print(namaa);
+}
