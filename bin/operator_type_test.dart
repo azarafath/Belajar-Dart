0099cc96a12c22dc848d37046ee3fe2de0b70fb2
azarafath <ahmadzakariafathoni@gmail.com>
2022-07-08 05:43:38 +0700

    type test

---
+++ bin/operator_type_test.dart
@@ -0,0 +1,12 @@
+void main() {
+  dynamic variable = 100;
+
+  var variableInt = variable as int;
+
+  print(variable);
+  print(variableInt);
+
+  print(variable is int);
+  print(variable is bool);
+  print(variableInt is String);
+}
