f16274240609cd54d575eadd5160b82c05176be3
azarafath <ahmadzakariafathoni@gmail.com>
2022-07-09 13:04:21 +0700

    update

---
+++ bin/map.dart
@@ -0,0 +1,13 @@
+void main() {
+  Map<String, String> map1 = {};
+  var map2 = Map<String, String>();
+  var map3 = <String, String>{};
+
+  print(map1);
+
+  var name = <String, String>{};
+  name['first'] = 'Ahmad';
+  name['second'] = 'Zakaria';
+
+  print(name);
+}
---
+++ bin/symbol.dart
@@ -0,0 +1,7 @@
+void main() {
+  Symbol symbol1 = Symbol('Ahmad Zakaria Fathoni');
+  var symbol2 = #belajar;
+
+  print(symbol1);
+  print(symbol2);
+}
